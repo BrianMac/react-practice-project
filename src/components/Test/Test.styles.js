62c76a3fffba21f56864fe717b1957a52054ec7b
BrianMac <87391503+BrianMac@users.noreply.github.com>
2022-03-24 12:07:44 -0400

    Initial Commit
    Initial Commit of React.js practice

---
+++ src/components/Test/Test.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    background: #fcba03;
+    padding: 10 20px;
+    border: 6px dashed #000;
+    margin: 30px;
+    `;
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: var(--maxWidth);
+    padding: 20px 0;
+    margin: 0 auto;
+`;
---
+++ src/components/Test/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper, Content } from './Test.styles';
+
+const Test = () => (
+    <Wrapper>
+        <Content>
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae, pariatur dolor? Nihil quisquam pariatur error minus tempora iste corporis ipsum soluta. Commodi unde reprehenderit facere nisi, a voluptates illum provident rerum quod, pariatur quam nobis accusantium quia optio laborum quis illo soluta qui voluptas aliquid dicta vitae? Corrupti, maiores veritatis.
+        </Content>
+    </Wrapper>
+);
+
+export default Test;
